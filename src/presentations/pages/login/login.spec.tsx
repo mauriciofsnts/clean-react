a031f61e65164dd36252c8b2483e18ed4610311f
Mauricio Ferraz dos Santos <mauriciofsnts@gmail.com>
2021-10-27 20:41:20 -0300

    test: ensure Login present valid state if password is valid

--- src/presentations/pages/login/login.spec.tsx
+++ src/presentations/pages/login/login.spec.tsx
@@ -88,4 +88,16 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe(validationSpy.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
   })
+
+  test('should show valid password state if validation succeeds', () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = sut.getByTestId('password-status')
+
+    expect(passwordStatus.title).toBe('Tudo certo')
+    expect(passwordStatus.textContent).toBe('🟢')
+  })
 })
