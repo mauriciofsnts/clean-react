5f114cf68de18114c6516afb8ee598aa2ddc7cae
Mauricio F <mauriciofsnts@gmail.com>
2021-10-30 14:07:19 -0300

    feat: ensure EmailValidation returns error if emails is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,11 +1,19 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 
 describe('EmailValidation', () => {
   test('should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return falsy if email is invalid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
