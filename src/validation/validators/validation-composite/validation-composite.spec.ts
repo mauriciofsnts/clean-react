d841b691a8b758ad8479ae71d5a881ffcd0149a1
Mauricio F <mauriciofsnts@gmail.com>
2021-10-30 16:55:30 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -6,10 +7,10 @@ type SutTypes = {
   fieldValidationSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
 
   const sut = new ValidationComposite(fieldValidationSpy)
@@ -22,12 +23,23 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
-    const { sut, fieldValidationSpy } = makeSut()
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationSpy } = makeSut(fieldName)
 
-    fieldValidationSpy[0].error = new Error('first_error_message')
-    fieldValidationSpy[1].error = new Error('second_error_message')
+    const erorMessage = faker.random.words()
 
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    fieldValidationSpy[0].error = new Error(erorMessage)
+    fieldValidationSpy[1].error = new Error(faker.random.words())
+
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(erorMessage)
+  })
+
+  test('should return falsy if there is no error', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
