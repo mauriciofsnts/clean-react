384d461d40fa775913efdc40dfc6b57321c650a2
Mauricio F <mauriciofsnts@gmail.com>
2021-12-06 22:39:17 -0300

    feat: add SignupValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,13 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignupValidation = (): ValidationComposite => {
+  const fieldValidationSpy = [
+    ...Builder.field('name').required().min(5).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation').required().min(5).build()
+  ]
+
+  return new ValidationComposite(fieldValidationSpy)
+}
