e115ada8b394929f1424fbe8d866a7a79fcf0651
Mauricio F <mauriciofsnts@gmail.com>
2022-01-12 14:24:51 -0300

    test: ensure signup prevent multiple submits

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -2,12 +2,16 @@ import faker from 'faker'
 import * as FormHelper from '../support/form-helper'
 import * as Http from '../support/signup-mocks'
 
-const simulateValidSubmit = (): void => {
+const populateFields = (): void => {
   const password = faker.random.alphaNumeric(5)
   cy.getByTestId('name').focus().type(faker.name.findName())
   cy.getByTestId('email').focus().type(faker.internet.email())
   cy.getByTestId('password').focus().type(password)
   cy.getByTestId('passwordConfirmation').focus().type(password)
+}
+
+const simulateValidSubmit = (): void => {
+  populateFields()
   cy.getByTestId('submit').click()
 }
 
@@ -105,5 +109,12 @@ describe('Signup', () => {
 
     FormHelper.testUrl('')
     FormHelper.testLocalStorageItem('access_token')
+  })
+
+  it('should prevent multiple submits', () => {
+    Http.mockOk()
+    populateFields()
+    cy.getByTestId('submit').dblclick()
+    FormHelper.testHttpCallsCount(1)
   })
 })
