c69d29c6f531cb91aa3d7d503a6f6cb99671c3b9
Mauricio F <mauriciofsnts@gmail.com>
2022-02-01 19:23:37 -0300

    feat: add survey icon to show status

--- src/presentations/pages/survey-list/survey-list.tsx
+++ src/presentations/pages/survey-list/survey-list.tsx
@@ -1,5 +1,5 @@
-import { Footer, Logo } from '@/presentations/components'
 import React from 'react'
+import { Footer, Logo } from '@/presentations/components'
 import Styles from './survey-list.scss'
 
 const SurveyList: React.FC = () => {
@@ -21,58 +21,12 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>04</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>04</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>04</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>04</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
+              <div className={[Styles.iconWrap, Styles.green].join(' ')}>
+                <img
+                  className={Styles.icon}
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC"
+                />
+              </div>
               <time>
                 <span className={Styles.day}>22</span>
                 <span className={Styles.month}>04</span>
