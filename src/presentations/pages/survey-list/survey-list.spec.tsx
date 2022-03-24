9ceb0e4490567686c2add91d1ef774f06c9d0290
Mauricio F <mauriciofsnts@gmail.com>
2022-03-23 22:05:47 -0300

    test: ensure LoadSurveyList component calls LoadSurveyList

--- src/presentations/pages/survey-list/survey-list.spec.tsx
+++ src/presentations/pages/survey-list/survey-list.spec.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentations/pages'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
-const makeSut = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList Component', () => {
@@ -12,5 +34,10 @@ describe('SurveyList Component', () => {
 
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
