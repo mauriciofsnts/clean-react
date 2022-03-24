1685684a943d244ac9e5f67fcb0f26959c6d0bd8
Mauricio F <mauriciofsnts@gmail.com>
2022-03-23 21:56:41 -0300

    refactor: move sut creation to a factory method

--- src/presentations/pages/survey-list/survey-list.spec.tsx
+++ src/presentations/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentations/pages'
 
+const makeSut = (): void => {
+  render(<SurveyList />)
+}
+
 describe('SurveyList Component', () => {
   test('should present 4 empty item on start', () => {
-    render(<SurveyList />)
+    makeSut()
 
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
