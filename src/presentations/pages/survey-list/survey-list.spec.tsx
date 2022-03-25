e634514916ff818938c6684e1b6f18759b94105d
Mauricio F <mauriciofsnts@gmail.com>
2022-03-25 20:15:06 -0300

    test: ensure SurveyList render error on failure

--- src/presentations/pages/survey-list/survey-list.spec.tsx
+++ src/presentations/pages/survey-list/survey-list.spec.tsx
@@ -6,6 +6,7 @@ import { SurveyModel } from '@/domain/models'
 import { mockSurveyListModel } from '@/domain/test'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory, MemoryHistory } from 'history'
+import { UnexpectedError } from '@/domain/errors'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
@@ -23,9 +24,8 @@ type SutTypes = {
   history: MemoryHistory
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
-  const loadSurveyListSpy = new LoadSurveyListSpy()
 
   render(
     <Router history={history}>
@@ -45,6 +45,7 @@ describe('SurveyList Component', () => {
 
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     await waitFor(() => surveyList)
   })
 
@@ -61,5 +62,19 @@ describe('SurveyList Component', () => {
     await waitFor(() => surveyList)
 
     expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(1)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+  })
+
+  test('should render error on failure', async () => {
+    const error = new UnexpectedError()
+    const loadSurveyListSpy = new LoadSurveyListSpy()
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(error)
+
+    makeSut(loadSurveyListSpy)
+
+    await waitFor(() => screen.getByRole('heading'))
+
+    expect(screen.queryByTestId('survey-list')).not.toBeInTheDocument()
+    expect(screen.getByTestId('error')).toHaveTextContent(error.message)
   })
 })
