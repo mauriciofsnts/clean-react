a1cf82d9bfa27c18d7c8018dd18c8892620c0a62
Mauricio F <mauriciofsnts@gmail.com>
2022-04-04 19:46:08 -0300

    feat: ensure AuthorizeHttpGetClientDecorator returns the same as httpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -22,7 +22,7 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
       }
     }
 
-    await this.httpGetClient.get(params)
-    return null
+    const httpResponse = await this.httpGetClient.get(params)
+    return httpResponse
   }
 }
