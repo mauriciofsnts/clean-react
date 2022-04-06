0ca6149e643c3587ae3ac6d0da47364e517aa425
Mauricio F <mauriciofsnts@gmail.com>
2022-04-06 19:58:47 -0300

    test: ensure LocalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter/local-storage-adapter.spec.ts
@@ -15,14 +15,26 @@ describe('LocalStorageAdapter', () => {
     const value = faker.random.objectElement<{}>()
 
     await sut.set(key, value)
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      key,
+      JSON.stringify(value)
+    )
+  })
+
+  test('should call localStorage.remove if value is undefined', async () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    await sut.set(key, undefined)
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
   })
 
   test('should call localStorage.get with correct value', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.objectElement<{}>()
-    const getSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+    const getSpy = jest
+      .spyOn(localStorage, 'getItem')
+      .mockReturnValueOnce(JSON.stringify(value))
 
     const obj = sut.get(key)
     expect(obj).toEqual(value)
