afa15a348fe00371835727fc12bb6cdf336fac7d
Mauricio F <mauriciofsnts@gmail.com>
2022-05-01 11:15:41 -0300

    refactor: move Calendar to its own Component

---
+++ src/presentations/components/calendar/calendar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Styles from './calendar-styles.scss'
+
+type Props = {
+  date: Date
+  className?: string
+}
+
+const Calendar: React.FC<Props> = ({ date, className }) => {
+  return (
+    <div className={[Styles.calendarWrap, className].join(' ')}>
+      <time>
+        <span data-testid="day" className={Styles.day}>
+          {date.getDate().toString().padStart(2, '0')}
+        </span>
+        <span data-testid="month" className={Styles.month}>
+          {date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}
+        </span>
+        <span data-testid="year" className={Styles.year}>
+          {date.getFullYear()}
+        </span>
+      </time>
+    </div>
+  )
+}
+
+export default Calendar
