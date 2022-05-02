e45f91e9a0ddf82872b40febba63d2002165f36f
Mauricio F <mauriciofsnts@gmail.com>
2022-05-02 20:17:05 -0300

    refactor: move SUT creation to a factory method

--- src/presentations/pages/survey-result/survey-result.spec.tsx
+++ src/presentations/pages/survey-result/survey-result.spec.tsx
@@ -4,19 +4,22 @@ import { render, screen } from '@testing-library/react'
 import { ApiContext } from '@/presentations/contexts'
 import { mockAccountModel } from '@/domain/test'
 
+const makeSut = (): void => {
+  render(
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: jest.fn(),
+        getCurrentAccount: () => mockAccountModel()
+      }}
+    >
+      <SurveyResult />
+    </ApiContext.Provider>
+  )
+}
+
 describe('SurveyResult Component', () => {
   test('should present correct initial state', () => {
-    render(
-      <ApiContext.Provider
-        value={{
-          setCurrentAccount: jest.fn(),
-          getCurrentAccount: () => mockAccountModel()
-        }}
-      >
-        <SurveyResult />
-      </ApiContext.Provider>
-    )
-
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
 
     expect(surveyResult.childElementCount).toBe(0)
